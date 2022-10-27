a4450ae756420e31cd2f9ad27bce68ecbdff64ca
Bob Tang <254202042@qq.com>
2022-10-27 21:35:55 +0800

    init

---
+++ hello.js
@@ -0,0 +1,5 @@
+var hello = function (name) {
+    return "hello ".concat(name);
+};
+console.log(hello('bob'));
+hello(1);
---
+++ hello.ts
@@ -0,0 +1,6 @@
+const hello = (name:string) => {
+    return `hello ${name}`
+}
+
+console.log(hello('bob'))
+hello(1)
